f516ad364b607d7e5e7df325b07d56be8f421e30
Julia Pottinger <jpottinger@JPottinger-MacBook-Pro.local>
2022-09-21 18:46:55 -0500

    seperated tests

--- test/updateBooking.js
+++ test/updateBooking.js
@@ -27,3 +27,49 @@ describe('Update Booking Scenario -', ()=>{
       expect(putResponse.body.additionalneeds).to.equal('Breakfast,Dinner')
     })
 })
+
+describe('Update Booking',() =>{
+  it('Verify that the booking is not updated with an invalid token', async ()=>{
+  
+    const url = `https://restful-booker.herokuapp.com/booking/76`
+
+    request(url)
+      .put('/')
+      .send({
+        "firstname" : "Julia",
+        "lastname" : "Browns",
+        "totalprice" : 567,
+        "depositpaid" : true,
+        "bookingdates" : {
+            "checkin" : "2020-01-01",
+            "checkout" : "2021-01-01"
+        },
+        "additionalneeds" : "Breakfast,Lunch"
+      })
+      .set('Accept', 'application/json')
+      .set('Authorization', 'Basic YWRtaW46cGFzc3dvcmQxMjM')
+      .expect(403)
+      .expect('Forbidden')
+  })
+
+  it('Verify that the booking is not updated with an invalid body', async ()=>{
+      const url = 'https://restful-booker.herokuapp.com/booking/10'
+
+      request(url)
+        .put('/')
+        .send({
+          "lastname" : "Potting",
+          "totalprice" : 111,
+          "depositpaid" : true,
+          "bookingdates" : {
+              "checkin" : "2018-01-01",
+              "checkout" : "2019-01-01"
+          },
+          "additionalneeds" : "Breakfast"
+        })
+        .set('Accept', 'application/json')
+        .set('Authorization', 'Basic YWRtaW46cGFzc3dvcmQxMjM=')
+        .expect(400)
+        .expect('Bad Request')
+  })
+})
